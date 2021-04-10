d64e9a7014d9b0c9668fa34357a636057ebc3344
Scenaristeur <scenrister@galcm>
2021-04-10 16:15:01 +0200

    path for gh-pages

---
+++ precache-manifest.8e39335b05df9e562c2a98606f1b1379.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75ed5a0c45524e731044",
+    "url": "/ipgs-chat/css/app.42ac9c9b.css"
+  },
+  {
+    "revision": "ba14cf0c1090ca64226e",
+    "url": "/ipgs-chat/css/chunk-vendors.267bdc3d.css"
+  },
+  {
+    "revision": "9f7bfd7fe13e35e719bdf1e51163bcf6",
+    "url": "/ipgs-chat/index.html"
+  },
+  {
+    "revision": "6ace58d9a0c352841463",
+    "url": "/ipgs-chat/js/about.56c4bd83.js"
+  },
+  {
+    "revision": "75ed5a0c45524e731044",
+    "url": "/ipgs-chat/js/app.21f5d2ff.js"
+  },
+  {
+    "revision": "d62333b9238cc19b577c",
+    "url": "/ipgs-chat/js/chunk-19b30634.5c1eb1cc.js"
+  },
+  {
+    "revision": "887f1457dc8858da3b63",
+    "url": "/ipgs-chat/js/chunk-c76a86e4.f425841f.js"
+  },
+  {
+    "revision": "e56e11a6a46bb226bde2",
+    "url": "/ipgs-chat/js/chunk-d353dcf2.aa492a41.js"
+  },
+  {
+    "revision": "ba14cf0c1090ca64226e",
+    "url": "/ipgs-chat/js/chunk-vendors.de814a70.js"
+  },
+  {
+    "revision": "5729a2da1c2cf8821db031772c81779c",
+    "url": "/ipgs-chat/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/ipgs-chat/robots.txt"
+  }
+]);
